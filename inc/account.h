04bf3076a640ac8351e8f637d369806f5379d8bb
victi <victini378@gmail.com>
2024-11-14 05:42:39 +0100

    Combination getting refactoring

--- inc/account.h
+++ inc/account.h
@@ -43,6 +43,8 @@ void init_account_file_data(struct File_Data *file_data);
 void get_current_account_file_data(struct File_Data *file_data);
 void unlink_all_memory_cards(char *title);
 
+char* get_user_combination(char* username);
+
 void display_account_details_short(struct Registry_Data *reg_data, int *no_user);
 void display_account_details_full(struct Registry_Data *reg_data, struct File_Data *file_data, char *title);
 void save_account_details(struct Registry_Data *reg_data, struct File_Data *file_data, char *title);
